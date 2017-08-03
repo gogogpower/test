3ad355e6199b4bec725d0d61a678145fca32add2
geek-engineer <kuotungtsai@kuotungdeMacBook-Pro.local>
2017-08-03 22:19:32 +0800

    reverser del

--- step_1/c-executable/src/main/c/main.c
+++ step_1/c-executable/src/main/c/main.c
@@ -1,7 +1,7 @@
-// #include <stdio.h>  
-// #include "greeting.h" 
+#include <stdio.h>  
+#include "greeting.h" 
 
-// int main(int argc, char** argv) {
-//     printf( "%s\n", GREETING_STRING);
-//     return 0;
-// }
+int main(int argc, char** argv) {
+    printf( "%s\n", GREETING_STRING);
+    return 0;
+}
